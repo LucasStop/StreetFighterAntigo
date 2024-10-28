e080bae4b89a571ace8a081ea078fb32e8d70387
Lucas Stopinski da Silva <lucasstopinskidasilva@gmail.com>
2024-10-27 22:30:59 -0300

    Classe Main com Interface Gráfica
    Primeiros passos na interface gráfica

---
+++ main/src/Main.java
@@ -0,0 +1,35 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Torneio.Torneio de Equipe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLayout(new GridBagLayout());
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        // Adiciona o JLabel
+        JLabel label = new JLabel("Torneio.Torneio de Equipe");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(label, gbc);
+
+        // Adiciona o JTextArea
+        JTextArea textArea = new JTextArea(5, 20); // Definido com altura e largura
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        panel.add(textArea, gbc);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
